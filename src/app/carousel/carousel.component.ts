8c81a21d46ee74a6e2540bfbccba53ce90356de1
Abigail18kiki <acrespin282@gmail.com>
2022-11-24 11:15:34 -0300

    subo cambio del carrosel

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -14,6 +14,48 @@ export class CarouselComponent implements OnInit {
       nombre:"cactus",
       precio:500,
       imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK57tcS_2XHieged9pHLbSm38Or1I97Kykpg&usqp=CAU",
+      descripcion:"es una familia de plantas originarias de América."
+    },
+    {
+      nombre:"Diente de Leon",
+      precio:600,
+      imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTasqPYRUa4SPCbdwYNBZygmHOS9hNPbGxWw0uuiSaIM0SjnvbWWGbwNzd-d94fJyVVbaE&usqp=CAU",
+      descripcion:"es una especie de la familia de las asteráceas."
+    },
+    {
+      nombre:"cactus",
+      precio:500,
+      imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK57tcS_2XHieged9pHLbSm38Or1I97Kykpg&usqp=CAU",
+      descripcion:"pincha"
+    },
+    {
+      nombre:"cactus",
+      precio:500,
+      imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK57tcS_2XHieged9pHLbSm38Or1I97Kykpg&usqp=CAU",
+      descripcion:"pincha"
+    },
+    {
+      nombre:"cactus",
+      precio:500,
+      imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK57tcS_2XHieged9pHLbSm38Or1I97Kykpg&usqp=CAU",
+      descripcion:"pincha"
+    },
+    {
+      nombre:"cactus",
+      precio:500,
+      imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK57tcS_2XHieged9pHLbSm38Or1I97Kykpg&usqp=CAU",
+      descripcion:"pincha"
+    }, 
+    {
+      nombre:"cactus",
+      precio:500,
+      imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK57tcS_2XHieged9pHLbSm38Or1I97Kykpg&usqp=CAU",
+      descripcion:"pincha"
+    },
+    {
+      nombre:"cactus",
+      precio:500,
+      imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK57tcS_2XHieged9pHLbSm38Or1I97Kykpg&usqp=CAU",
       descripcion:"pincha"
     },
     {
