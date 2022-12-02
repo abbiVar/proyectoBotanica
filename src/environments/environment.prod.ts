6b5457c74358fed6d7d1866c77172c782089c95d
erickzurita13 <erickzurita010816@gmail.com>
2022-12-02 10:20:30 -0300

    se agrego firebase al proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'botanica-558ac',
+    appId: '1:397003953610:web:b523ebc986413b1f7eeb33',
+    storageBucket: 'botanica-558ac.appspot.com',
+    apiKey: 'AIzaSyAd3GYYeV1S2QFg38VJvl0roYCwElqgCXI',
+    authDomain: 'botanica-558ac.firebaseapp.com',
+    messagingSenderId: '397003953610',
+    measurementId: 'G-4LNYNWGMED',
+  },
   production: true
 };
